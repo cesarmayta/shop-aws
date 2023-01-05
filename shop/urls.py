569bb95bfbae849a454cf0810852a70e56a25f3f
César Mayta <cesarmayta@gmail.com>
2023-01-05 00:37:14 -0500

    urls cambiado

--- shop/urls.py
+++ shop/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('',include('web.urls')),
     path('api/',include('api.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
